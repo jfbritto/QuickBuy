7a193ffdf52dd2b62c1e75bed4bae8900c1cee7c
João Filipi Britto <jf.britto@hotmail.com>
2020-01-19 17:01:49 -0300

    Autenticação na tela

--- QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,28 +1,25 @@
 import { Component } from "@angular/core";
 import { Usuario } from "../../model/usuario";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.css"]
 })
+
 export class LoginComponent {
 
   public usuario;
-  public usuarioAutenticado = false;
-  public usuarioIncorreto = false;
 
-  constructor() {
+  constructor(private router: Router) {
     this.usuario = new Usuario();
   }
   
   entrar() {
     if (this.usuario.email == "jf.britto@hotmail.com" && this.usuario.senha == "123") {
-      this.usuarioAutenticado = true;
-      this.usuarioIncorreto = false;
-    } else {
-      this.usuarioIncorreto = true;
-      this.usuarioAutenticado = false;
+      sessionStorage.setItem("usuario-autenticado", "1");
+      this.router.navigate(['/']);
     }
   }
 
