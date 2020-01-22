a5ff8a557aba6de2ab19ab1c106e6bc34abc79b8
João Filipi Britto <jf.britto@hotmail.com>
2020-01-21 23:06:17 -0300

    Criando tela de cadastro de produtos

--- QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
+++ QuickBuy.Web/ClientApp/src/app/produto/produto.component.ts
@@ -1,17 +1,37 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { Produto } from "../model/produto";
+import { ProdutoServico } from "../servicos/produto/produto.servico";
 
 @Component({
   selector: "app-produto",
-  template: "<html><body>{{ obterNome() }}</body></html>"
+  templateUrl: "./produto.component.html",
+  styleUrls: ["./produto.component.css"]
 })
-export class ProdutoComponent {
-  /*camelCase para atributos e nomes de funções*/
-  public nome: string;
-  public liberadoParaVenda: boolean;
+export class ProdutoComponent implements OnInit {
 
-  public obterNome(): string {
-    /*return this.nome;*/
-    return "Samsung";
+  public produto: Produto;
+  private ativar_spinner: boolean;
+
+  constructor(private produtoServico: ProdutoServico) {
+
+  }
+
+  ngOnInit(): void {
+    this.produto = new Produto();
+  }
+
+  public cadastrar() {
+    this.ativar_spinner = true;
+    this.produtoServico.cadastrar(this.produto)
+      .subscribe(
+        produto_json => {
+          console.log(produto_json);
+        },
+        err => {
+          console.log(err.error);
+          this.ativar_spinner = false;
+        }
+      );
   }
 
 }
